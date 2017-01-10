d00ef413b75a161f31ec9cc85a5b8db038ce6157
Robin Radic <robin@radic.nl>
2017-01-10 12:18:53 +0100

    improved speed by caching addon resolver results

---
+++ src/WelcomePlugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Part of the Codex Project packages.
+ *
+ * License and copyright information bundled with this package in the LICENSE file.
+ *
+ * @author Robin Radic
+ * @copyright Copyright 2017 (c) Codex Project
+ * @license http://codex-project.ninja/license The MIT License
+ */
+namespace Codex\Addon\Welcome;
+
+use Codex\Addons\Annotations as CA;
+use Codex\Addons\BasePlugin;
+
+/**
+ * This is the class WelcomePlugin.
+ *
+ * @package        Codex\Addon
+ * @author         CLI
+ * @copyright      Copyright (c) 2015, CLI. All rights reserved
+ * @CA\Plugin("welcome", description="Welcome page")
+ */
+class WelcomePlugin extends BasePlugin
+{
+
+}
